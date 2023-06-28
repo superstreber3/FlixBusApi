2360eda00f448f26c64b1606b530e41a30229bc6
Silas <silas.zahner@gmail.com>
2023-06-28 20:30:15 +0200

    changed to from mssql to sqllite

--- main.js
+++ main.js
@@ -1,31 +1,57 @@
 const express = require('express');
-const sql = require('mssql');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const csv = require('csv-parser');
 const fs = require('fs');
 
-// Connection string parameters.
-var config = {
-    user: 'Flixbus',
-    password: 'Flixbus123',
-    server: 'localhost',
-    database: 'Flixbus',
-    options: {
-        encrypt: false
+let db = new sqlite3.Database('./Flixbus.db', (err) => {
+    if (err) {
+        console.error(err.message);
     }
-};
+    console.log('Connected to the Flixbus database.');
 
-sql.connect(config, function (err) {
-    if (err) console.log(err);
+    db.serialize(() => {
+        db.run(`
+        CREATE TABLE IF NOT EXISTS Customer (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstName TEXT,
+            lastName TEXT,
+            email TEXT
+        );
+        `);
 
-    let request = new sql.Request();
+        db.run(`
+        CREATE TABLE IF NOT EXISTS Station (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            longitude REAL,
+            latitude REAL,
+            address TEXT,
+            country TEXT,
+            zip TEXT,
+            city TEXT
+        );
+        `);
+
+        db.run(`
+        CREATE TABLE IF NOT EXISTS Tour (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            startStationId INTEGER,
+            endStationId INTEGER,
+            customerId INTEGER,
+            FOREIGN KEY (startStationId) REFERENCES Station(id),
+            FOREIGN KEY (endStationId) REFERENCES Station(id),
+            FOREIGN KEY (customerId) REFERENCES Customer(id)
+        );
+        `);
+    });
 
     fs.createReadStream('./db/stations.csv')
         .pipe(csv())
         .on('data', (row) => {
-            let insertQuery = `IF NOT EXISTS (SELECT * FROM Station WHERE name='${row.name}') INSERT INTO Station(name, longitude, latitude, address, country, zip, city) VALUES('${row.name}', ${row.longitude}, ${row.latitude}, '${row.address}', '${row.country}', '${row.zip}', '${row.city}')`;
+            let insertQuery = `INSERT INTO Station(name, longitude, latitude, address, country, zip, city) SELECT '${row.name}', ${row.longitude}, ${row.latitude}, '${row.address}', '${row.country}', '${row.zip}', '${row.city}' WHERE NOT EXISTS (SELECT 1 FROM Station WHERE name='${row.name}')`;
 
-            request.query(insertQuery, function (err, result) {
+            db.run(insertQuery, function (err) {
                 if (err) {
                     console.log(err);
                 }
@@ -36,119 +62,82 @@ sql.connect(config, function (err) {
         });
 });
 
-
 app.use(express.json());
 
 app.post('/createStation', function (req, res) {
-    sql.connect(config, function (err) {
-        if (err) console.log(err);
+    let insertQuery = `INSERT INTO Station(name, longitude, latitude, address, country, zip, city) VALUES(?, ?, ?, ?, ?, ?, ?)`;
 
-        let request = new sql.Request();
-
-        let insertQuery = `INSERT INTO Station(name, longitude, latitude, address, country, zip, city) VALUES('${req.body.name}', ${req.body.longitude}, ${req.body.latitude}, '${req.body.address}', '${req.body.country}', '${req.body.zip}', '${req.body.city}')`;
-
-        request.query(insertQuery, function (err, result) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            }
-            else {
-                res.status(200).send({ message: "Station created successfully." });
-            }
-        });
+    db.run(insertQuery, [req.body.name, req.body.longitude, req.body.latitude, req.body.address, req.body.country, req.body.zip, req.body.city], function (err) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.status(200).send({ message: "Station created successfully." });
+        }
     });
 });
 
 app.post('/createTour', function (req, res) {
-    sql.connect(config, function (err) {
-        if (err) console.log(err);
+    let insertCustomerQuery = `INSERT INTO Customer(firstName, lastName, email) VALUES(?, ?, ?)`;
 
-        let request = new sql.Request();
+    db.run(insertCustomerQuery, [req.body.firstName, req.body.lastName, req.body.email], function (err) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            let customerId = this.lastID;
+            let insertTourQuery = `INSERT INTO Tour(startStationId, endStationId, customerId) VALUES(?, ?, ?)`;
 
-        let insertCustomerQuery = `INSERT INTO Customer(firstName, lastName, email) OUTPUT INSERTED.ID VALUES('${req.body.firstName}', '${req.body.lastName}', '${req.body.email}')`;
-
-        request.query(insertCustomerQuery, function (err, customerResult) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            }
-            else {
-                let insertTourQuery = `INSERT INTO Tour(startStationId, endStationId, customerId) VALUES('${req.body.startStationId}', '${req.body.endStationId}', '${customerResult.recordset[0].ID}')`;
-
-                request.query(insertTourQuery, function (err, tourResult) {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).send(err);
-                    }
-                    else {
-                        res.status(200).send({ message: "Tour created successfully." });
-                    }
-                });
-            }
-        });
+            db.run(insertTourQuery, [req.body.startStationId, req.body.endStationId, customerId], function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                } else {
+                    res.status(200).send({ message: "Tour created successfully." });
+                }
+            });
+        }
     });
 });
 
 app.get('/tours', function (req, res) {
-    sql.connect(config, function (err) {
-        if (err) console.log(err);
+    let selectQuery = `SELECT Tour.*, startStation.name as startStation, endStation.name as endStation FROM Tour JOIN Station as startStation ON Tour.startStationId = startStation.id JOIN Station as endStation ON Tour.endStationId = endStation.id`;
 
-        let request = new sql.Request();
-
-        let selectQuery = `SELECT Tour.*, startStation.name as startStation, endStation.name as endStation FROM Tour JOIN Station as startStation ON Tour.startStationId = startStation.id JOIN Station as endStation ON Tour.endStationId = endStation.id`;
-
-        request.query(selectQuery, function (err, result) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            }
-            else {
-                res.status(200).send(result.recordset);
-            }
-        });
+    db.all(selectQuery, function (err, rows) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(rows);
+        }
     });
 });
 
 app.get('/tours/:email', function (req, res) {
-    sql.connect(config, function (err) {
-        if (err) console.log(err);
+    let selectQuery = `SELECT Tour.*, startStation.name as startStation, endStation.name as endStation FROM Tour JOIN Station as startStation ON Tour.startStationId = startStation.id JOIN Station as endStation ON Tour.endStationId = endStation.id JOIN Customer ON Tour.customerId = Customer.id WHERE Customer.email = ?`;
 
-        let request = new sql.Request();
-
-        let selectQuery = `SELECT Tour.*, startStation.name as startStation, endStation.name as endStation FROM Tour JOIN Station as startStation ON Tour.startStationId = startStation.id JOIN Station as endStation ON Tour.endStationId = endStation.id JOIN Customer ON Tour.customerId = Customer.id WHERE Customer.email = '${req.params.email}'`;
-
-        request.query(selectQuery, function (err, result) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            }
-            else {
-                res.status(200).send(result.recordset);
-            }
-        });
+    db.all(selectQuery, [req.params.email], function (err, rows) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(rows);
+        }
     });
 });
 
 app.get('/stations', function (req, res) {
-    sql.connect(config, function (err) {
-        if (err) console.log(err);
+    let selectQuery = `SELECT * FROM Station`;
 
-        let request = new sql.Request();
-
-        let selectQuery = `SELECT * FROM Station`;
-
-        request.query(selectQuery, function (err, result) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            }
-            else {
-                res.status(200).send(result.recordset);
-            }
-        });
+    db.all(selectQuery, function (err, rows) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(rows);
+        }
     });
 });
-
 
 app.listen(1221, function () {
     console.log('Server is running on port 1221..');
